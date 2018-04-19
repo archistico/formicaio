106a69bae5fbb56c88e5a40ea4a09208cfc59800
archistico <info@archistico.com>
2018-04-19 22:24:17 +0200

    add mappa

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+let secondo = 0;
+
 function setup() {
-    formicaio = new Formicaio(300, 100, 10);
+    createCanvas(505, 505);
+
+    formicaio = new Formicaio(50, 50, 10);
     formica1 = new Formica(formicaio);
     formica2 = new Formica(formicaio);
     formica3 = new Formica(formicaio);
@@ -10,13 +14,23 @@ function setup() {
     formica8 = new Formica(formicaio);
     formica9 = new Formica(formicaio);
     formica10 = new Formica(formicaio);
+
+    mappa = new Mappa(100, 100);
 }
 
 function draw() {
     //ellipse(50, 50, 80, 80);
     formicaio.formiche.forEach(function(formica) {
-        formica.vivi();
+        formica.vivi(mappa);
     });
+
+    mappa.disegna();
+
+    secondo++;
+}
+
+function tira(min, max) {
+    return Math.floor((Math.random() * max) + min);
 }
 
 class Formicaio {
@@ -60,19 +74,107 @@ class Formica {
         this.id = formicaio.getCount();
         this.formicaio.addFormica(this);
 
-        this.maxVita = 86400 * 3;
+        // variabili generali formiche
+        this.maxEta = 86400 * 365 * 5; // 3 giorni
+        this.maxFame = 86400 * 10;
+        this.maxSete = 86400 * 3;
+        this.tick = this.maxEta / 10000;
     }
 
-    vivi() {
-        if (this.salute <= 0) {
+    vivi(mappa) {
+        if ((this.salute <= 0) || (this.eta >= this.maxEta)) {
             this.formicaio.removeFormica(this);
         }
-        this.salute -= 86400;
-        this.eta += 1;
+
+        // Controlla se la casella in cui sono c'è cibo o acqua
+        // se si metti fame o sete a 100
+        if (mappa.checkAcqua(this.x, this.y)) {
+            this.sete = 100;
+        }
+        if (mappa.checkCibo(this.x, this.y)) {
+            this.fame = 100;
+        }
+
+        // Controlla fame e sete per capire la salute
+        //this.salute -= 1;
+
+        // Aumenta l'età
+        this.eta += this.tick;
     }
 
     // Azioni formica
     /**
      *  
      */
+}
+
+class Mappa {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+
+        this.caselle = w * h;
+        this.terreno = [];
+
+        for (let i = 0; i < h; i++) {
+            // per ogni riga
+            this.terreno[i] = [];
+        }
+
+        for (let r = 0; r < h; r++) {
+            for (let c = 0; c < w; c++) {
+                this.terreno[c][r] = '';
+            }
+        }
+
+        let nCibo = this.caselle / 100;
+        let nAcqua = this.caselle / 200;
+
+        for (let i = 0; i < nCibo; i++) {
+            this.terreno[tira(0, w)][tira(0, h)] = 'C';
+        }
+
+        for (let i = 0; i < nAcqua; i++) {
+            this.terreno[tira(0, w)][tira(0, h)] = 'H';
+        }
+
+    }
+
+    checkAcqua(x, y) {
+        if (this.terreno[x][y] == 'H') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    checkCibo(x, y) {
+        if (this.terreno[x][y] == 'C') {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    disegna() {
+
+        for (let r = 0; r < this.height; r++) {
+            for (let c = 0; c < this.width; c++) {
+                switch (this.terreno[c][r]) {
+                    case '':
+                        fill(0, 0, 0);
+                        break;
+                    case 'H':
+                        fill(0, 0, 255);
+                        break;
+                    case 'C':
+                        fill(0, 255, 0);
+                        break;
+                }
+                rect(c * 5, r * 5, 5, 5);
+            }
+        }
+
+        //rect(0, 0, this.width * 5, this.height * 5);
+    }
 }
