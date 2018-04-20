5aaa3fc364fca438b3a31fe39e7d32b0b4480668
archistico <info@archistico.com>
2018-04-20 09:31:57 +0000

    aggiunta verifica fame, sete

--- js/app.js
+++ js/app.js
@@ -15,7 +15,7 @@ function setup() {
     formica8 = new Formica(formicaio);
     formica9 = new Formica(formicaio);
     formica10 = new Formica(formicaio);
-
+    
     mappa = new Mappa(100, 50);
 }
 
@@ -59,7 +59,21 @@ class Formicaio {
         if (id > -1) {
             this.formiche.splice(id, 1);
         }
-        console.log("Formica con id: " + formica.id + " rimossa dal formicaio");
+
+        // Causa della morte: di fame, di sete, di fame e di sete
+        let causa = '';
+        if(causa=='' && formica.fame == 0 && formica.sete == 0) {
+            causa = 'di fame e di sete';
+        }
+        if(causa=='' && formica.fame != 0 && formica.sete == 0) {
+            causa = 'di sete';
+        }
+        if(causa=='' && formica.fame == 0 && formica.sete != 0) {
+            causa = 'di fame';
+        }
+
+        console.log("Formica con id:" + formica.id + " rimossa dal formicaio perchè morta "+causa);
+        console.log("- Dati: "+formica.getDati());
     }
 }
 
@@ -76,10 +90,63 @@ class Formica {
         this.formicaio.addFormica(this);
 
         // variabili generali formiche
-        this.maxEta = 86400 * 365 * 5; // 3 giorni
-        this.maxFame = 86400 * 10;
-        this.maxSete = 86400 * 3;
-        this.tick = this.maxEta / 10000;
+        this.giorno = 1440;
+        this.maxEta = this.giorno * 365; // 3 giorni
+        this.tick = this.maxEta/365;
+
+        // Genoma movimento
+        // A avanti
+        // I indietro
+        // D destra
+        // S sinistra
+        this.genoma = [];
+
+        // Randomizza genoma
+        for(let i=0; i<50; i++) {
+            let mov = tira(1, 4);
+            switch(mov) {
+                case 1: this.genoma.push("A"); break;
+                case 2: this.genoma.push("I"); break;
+                case 3: this.genoma.push("D"); break;
+                case 4: this.genoma.push("S"); break;
+            }
+        }
+
+        this.position = 0;
+    }
+
+    riduciSalute(value) {
+        
+        this.salute -= value;
+
+        // controlla limiti
+        if(this.salute > 100) {
+            this.salute = 100;
+        }
+        if(this.salute < 0) {
+            this.salute = 0;
+        }
+    }
+
+    aumentaSalute(value) {
+        
+        this.salute += value;
+
+        // controlla limiti
+        if(this.salute > 100) {
+            this.salute = 100;
+        }
+        if(this.salute < 0) {
+            this.salute = 0;
+        }
+    }
+
+    getGenoma() {
+        return this.genoma;
+    }
+
+    getGenomaStr() {
+        return this.genoma.toString();
     }
 
     vivi(mappa) {
@@ -87,26 +154,96 @@ class Formica {
             this.formicaio.removeFormica(this);
         }
 
+        // Muoviti
+        if(this.position > (this.genoma.length-1)) {
+            this.position = 0;
+        }
+        switch(this.genoma[this.position]) {
+            case "A": this.y+=1; break;
+            case "I": this.y-=1; break;
+            case "D": this.x+=1; break;
+            case "S": this.x-=1; break;
+        }
+        this.position++;
+
+        if(this.x >= mappa.width-1) {
+            this.x = mappa.width-1;
+        }
+        if(this.y >= mappa.height-1) {
+            this.y = mappa.height-1;
+        }
+
+        if(this.x <= 0) {
+            this.x = 0;
+        }
+        if(this.y <= 0) {
+            this.y = 0;
+        }
+
         // Controlla se la casella in cui sono c'è cibo o acqua
         // se si metti fame o sete a 100
-        if (mappa.checkAcqua(this.x, this.y)) {
+        let checkAcqua = mappa.checkAcqua(this.x, this.y);
+        let checkCibo = mappa.checkCibo(this.x, this.y);
+        if (checkAcqua) {
             this.sete = 100;
+            console.log("Formica ID: " + this.id + " ha bevuto");            
+        } else {
+            if(this.sete>0) {
+                this.sete -= 1;
+            } else {
+                this.sete = 0;
+            }
         }
-        if (mappa.checkCibo(this.x, this.y)) {
+        if (checkCibo) {
             this.fame = 100;
+            console.log("Formica ID: " + this.id + " ha mangiato");
+        } else {
+            if(this.fame>0) {
+                this.fame -= 1;
+            } else {
+                this.fame = 0;
+            }
+        }
+         
+        // ATTENZIONE CHE VENGONO PRESE TUTTE ASSIEME
+        let checkSalute = false;
+
+        // Se ho sia fame che sete la salute scende ancora più rapidamente
+        if((this.sete<=0) && (this.fame<=0) && !checkSalute) {
+            checkSalute = true;
+            this.riduciSalute(10);
+        }
+
+        // La sete fa scendere la salute più velocemente
+        if((this.sete<=0) && !checkSalute) {
+            checkSalute = true;
+            this.riduciSalute(3);
         }
 
         // Controlla fame e sete per capire la salute
-        //this.salute -= 1;
+        if((this.fame<=0) && !checkSalute) {
+            checkSalute = true;
+            this.riduciSalute(1);
+        }
+        
+
+        // Se sto bene, cioè non ho fame o sete, la salute risale
+        if((this.sete>0) && (this.fame>0) && !checkSalute) {
+            checkSalute = true;
+            this.aumentaSalute(1);
+        }
 
         // Aumenta l'età
         this.eta += this.tick;
     }
 
-    // Azioni formica
-    /**
-     *  
-     */
+    performance() {
+        return this.eta.toFixed(2);
+    }
+
+    getDati() {
+        return "ID:" + this.id + " | Età:"+ this.eta.toFixed(2) + " | Età max:"+ this.maxEta.toFixed(2) +" | Salute:" + this.salute + " | Fame:" + this.fame + " | Sete:" + this.sete+ " | Genoma:" + this.getGenomaStr();
+    }
 }
 
 class Mappa {
@@ -129,7 +266,7 @@ class Mappa {
         }
 
         let nCibo = this.caselle / 100;
-        let nAcqua = this.caselle / 200;
+        let nAcqua = this.caselle / 100;
 
         for (let i = 0; i < nCibo; i++) {
             this.terreno[tira(0, w)][tira(0, h)] = 'C';
